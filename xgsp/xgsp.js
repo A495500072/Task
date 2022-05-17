9aa785092dc0c1dda379fc62e50a2571d861a008
XWF888 <77550098+XWF888@users.noreply.github.com>
2022-05-17 09:48:34 +0800

    Update xgsp.js

--- xgsp/xgsp.js
+++ xgsp/xgsp.js
@@ -48,14 +48,13 @@ let XGSPjinehd = ($.isNode() ? process.env.XGSPjinehd :  $.getdata('XGSPjinehd')
 
 
 
-/*//西瓜转圈圈视频
-let XGSPzqurl = ($.isNode() ? process.env.XGSPzqurl :  $.getdata('XGSPzqurl')) || '';
-let XGSPzqhd = ($.isNode() ? 
-process.env.XGSPzqhd :    $.getdata('XGSPzqhd')) || '';
-let XGSPzqbody = ($.isNode() ?process.env.XGSPzqbody : $.getdata('XGSPzqbody')) || '';
+//西瓜直播
+let XGSPliveurl = ($.isNode() ? process.env.XGSPliveurl :  $.getdata('XGSPliveurl')) || '';
+let XGSPlivehd = ($.isNode() ? 
+process.env.XGSPlivehd :    $.getdata('XGSPlivehd')) || '';
 
 
-//西瓜直播宝箱
+/*//西瓜直播宝箱
 let XGSPzbbxurl = ($.isNode() ? process.env.XGSPzbbxurl : $.getdata('XGSPzbbxurl')) || '';
 let XGSPzbbxhd = ($.isNode() ? 
 process.env.XGSPzbbxhd : $.getdata('XGSPzbbxhd')) || '';
@@ -94,6 +93,9 @@ let XGSPzbbxbody = ($.isNode() ?process.env.XGSPzbbxbody:$.getdata('XGSPzbbxbody
 
     XGSPjineurlArr = XGSPjineurl.split('@')
     XGSPjinehdArr = XGSPjinehd.split('@')
+    
+    XGSPliveurlArr = XGSPliveurl.split('@')
+    XGSPlivehdArr = XGSPlivehd.split('@')
 
     console.log(`      ------------- 共${XGSPhdArr.length}个账号-------------\n`)
     for (let i = 0; i < XGSPhdArr.length; i++) {
@@ -122,6 +124,9 @@ let XGSPzbbxbody = ($.isNode() ?process.env.XGSPzbbxbody:$.getdata('XGSPzbbxbody
       XGSPjineurl = XGSPjineurlArr[i]
       XGSPjinehd = XGSPjinehdArr[i]
 
+      XGSPliveurl = XGSPliveurlArr[i]
+      XGSPlivehd = XGSPlivehdArr[i]
+
 
       $.index = i + 1;
       
@@ -131,24 +136,24 @@ console.log(`\n\n===============================================                
  
             await xgyhm()
             await $.wait(1000)
-            await xgqd()//你要执行的版块
-            await $.wait(1000)//你要延迟时间1000=1秒
+            //await xgqd()//你要执行的版块
+            //await $.wait(1000)//你要延迟时间1000=1秒
 //循环运行多次任务
-  for (let c = 0; c < 5; c++) {
+  for (let c = 0; c < 1; c++) {
            $.index = c + 1
-            await xgyd()  
-            await $.wait(28000)
+            //await xgyd()  
+            //await $.wait(28000)
       }
-            await xgkbx()
-            await $.wait(10000)
-            await xgbxgg()
-            await $.wait(20000)
+            //await xgkbx()
+            //await $.wait(10000)
+            //await xgbxgg()
+            //await $.wait(20000)
             await xgbx1()
             await $.wait(5000)
             await xgbx2()
             await $.wait(15000)
-            await xgggjf()
-            await $.wait(15000)
+            await xglive()
+            await $.wait(5000)
             //await dyzbkbx()
 //循环运行多次任务
   for (let c = 0; c < 5; c++) {
@@ -199,7 +204,7 @@ function XGSPck() {
 
 
 
-/*//右下角开宝箱
+//开宝箱
     if ($request.url.indexOf("coin_chest") > -1) {
         const XGSPkbxurl = $request.url
         if (XGSPkbxurl) $.setdata(XGSPkbxurl, `XGSPkbxurl${status}`)
@@ -211,10 +216,9 @@ function XGSPck() {
 
         $.msg($.name, "", `西瓜${status}获取XGSPkbxhd成功`)
 
-    }*/
+    }    
 
-
-//宝箱看广告     
+/*//宝箱看广告     
     if ($request.url.indexOf("chest_ad") > -1) {
         const XGSPbxggurl = $request.url
         if (XGSPbxggurl) $.setdata(XGSPbxggurl, `XGSPbxggurl${status}`)
@@ -226,10 +230,10 @@ function XGSPck() {
 
         $.msg($.name, "", `西瓜${status}获取XGSPbxgghd成功`)//$.msg这段是通知提示信息获取成功
 
-    }
+    }*/
   
     
-//宝箱再得广告1
+//宝箱1
    if ($request.url.indexOf("task/query/ad_watch_daily_again") > -1) {
         const XGSPbx1url = $request.url
         if (XGSPbx1url) $.setdata(XGSPbx1url, `XGSPbx1url${status}`)
@@ -244,7 +248,7 @@ function XGSPck() {
     }
 
  
-//宝箱再得广告2
+//宝箱2
     if ($request.url.indexOf("task/done/ad_watch_daily_again") > -1) {
         const XGSPbx2url = $request.url
         if (XGSPbx2url) $.setdata(XGSPbx2url, `XGSPbx2url${status}`)
@@ -263,7 +267,7 @@ function XGSPck() {
     }
 
 
-//看广告赚积分     
+//广告积分     
     if ($request.url.indexOf("task/done/ad_watch_daily") > -1) {
         const XGSPggjfurl = $request.url
         if (XGSPggjfurl) $.setdata(XGSPggjfurl, `XGSPggjfurl${status}`)
@@ -277,7 +281,7 @@ function XGSPck() {
 
     }
 
-//西瓜金币收益     
+//西瓜金额     
     if ($request.url.indexOf("task/page") > -1) {
         const XGSPjineurl = $request.url
         if (XGSPjineurl) $.setdata(XGSPjineurl, `XGSPjineurl${status}`)
@@ -288,6 +292,20 @@ function XGSPck() {
         $.log(XGSPjinehd)
 
         $.msg($.name, "", `西瓜${status}获取XGSPjinehd成功`)//$.msg这段是通知提示信息获取成功
+
+    }
+
+//西瓜直播
+    if ($request.url.indexOf("live") > -1) {
+        const XGSPliveurl = $request.url
+        if (XGSPliveurl) $.setdata(XGSPliveurl, `XGSPliveurl${status}`)
+        $.log(XGSPliveurl)
+
+        const XGSPlivehd = JSON.stringify($request.headers)
+        if (XGSPlivehd) $.setdata(XGSPlivehd, `XGSPlivehd${status}`)
+        $.log(XGSPlivehd)
+
+        $.msg($.name, "", `西瓜${status}获取XGSPlivehd成功`)//$.msg这段是通知提示信息获取成功
 
     }
 
@@ -315,12 +333,12 @@ function xgqd(timeout = 0) {
 
                 if (data.err_no == 0) {
             
-            console.log(`📣🎉西瓜视频签到：${data.err_tips}`)//打印返回成功数值
+            console.log(`🎉西瓜视频签到：${data.err_tips}`)//打印返回成功数值
             
                 //else是不然的意思
                 } else {
 
-            console.log(`✅🙅西瓜视频已签到：${data.date}😿😿`)//打印返回失败数值
+            console.log(`❌西瓜视频已签到：${data.date}😿😿😿`)//打印返回失败数值
                 }
             } catch (e) {
 
@@ -350,12 +368,12 @@ function xgyd(timeout = 0) {
 
                 if (data.err_no == 0) {
             
-            console.log(`📣🎉阅读获取：${data.data.daily_watch_result.amount}金币`)//打印返回成功数值
+            console.log(`🎉阅读获取：${data.data.daily_watch_result.amount}金币`)//打印返回成功数值
 
                 //else是不然的意思
                 } else {
 
-            console.log(`❌🙅阅读失败：${data.err_tips}😿😿`)//打印返回失败数值
+            console.log(`❌阅读失败：${data.err_tips}😿😿😿`)//打印返回失败数值
                 }
             } catch (e) {
 
@@ -383,12 +401,12 @@ function xgkbx(timeout = 0) {
 
                 if (data.err_no == 0) {
             
-            console.log(`📣🎉开宝箱获取：${data.data.amount}金币`)//打印返回成功数值
+            console.log(`🎉开宝箱获取：${data.data.amount}金币`)//打印返回成功数值
 
                 //else是不然的意思
                 } else {
 
-            //console.log(`❌🙅开宝箱失败：${data.err_tips}😿😿`)//打印返回失败数值
+            console.log()//打印返回失败数值
                 }
             } catch (e) {
 
@@ -415,10 +433,10 @@ function xgbxgg(timeout = 0) {
                 data = JSON.parse(data)
 
                 if (data.err_no == 0) {
-    console.log(`📣🎉宝箱广告成功：${data.data.amount}金币`)//打印返回成功數值
+    console.log(`🎉广告金币成功：${data.data.amount}金币`)//打印返回成功數值
            
                } else {
-    console.log(`❌🙅宝箱广告失败：${data.err_tips}😿😿`)//打印返回失败数值
+    console.log(`❌广告失败：${data.err_tips}😿😿`)//打印返回失败数值
 
                 }
             } catch (e) {
@@ -447,11 +465,11 @@ function xgbx1(timeout = 0) {
 
                 if (data.err_no == 0) {
             
-            console.log(`📣🎉宝箱广告1成功：${data.data.amount}金币`)//打印返回成功數值
+            console.log(`🎉宝箱刷新金币：${data.data.amount}金币`)//打印返回成功數值
                 //else是不然的意思
                 } else {
 
-            console.log(`❌🙅宝箱广告1失败：${data.err_tips}😿😿`)//打印返回失败数值
+            //打印返回失敗數值
                 }
             } catch (e) {
 
@@ -478,10 +496,10 @@ function xgbx2(timeout = 0) {
                 data = JSON.parse(data)
 
                 if (data.err_no == 0) {
-    console.log(`📣🎉宝箱广告2成功：${data.data.amount}金币`)//打印返回成功數值
+    console.log(`🎉获取金币成功：${data.data.amount}金币`)//打印返回成功數值
            
                } else {
-    console.log(`❌🙅宝箱广告2失败：${data.err_tips} 😿😿 `)//打印返回失败数值
+    console.log(`❌获取金币失败：${data.err_tips}`)//打印返回失败数值
 
                 }
             } catch (e) {
@@ -509,8 +527,8 @@ function xgggjf(timeout = 0) {
                 data = JSON.parse(data)
 
                 if (data.err_no == 0) {
-    console.log(`📣🎉广告赚积分：${data.err_tips}`)//打印返回成功數值
-           console.log(`📣🎉广告赚积分收取：${data.data.amount}金币`)
+    console.log(`🎉广告赚积分：${data.err_tips}`)//打印返回成功數值
+           console.log(`🎉广告赚积分收取：${data.data.amount}金币`)
                } else {
 
                 }
@@ -543,9 +561,9 @@ function xgyhm(timeout = 0) {
     
        console.log(`==============用户名：${data.data.user_info.name}==============`)
 
-       console.log(`📣🎉我的金额💰：${data.data.income_info.cash_balance}分`)  
+       console.log(`🎉我的金额💰：${data.data.income_info.cash_balance}分`)  
 
-       console.log(`📣🎉我的积分：${data.data.income_info.score_balance}金币`)//打印返回成功數值        
+       console.log(`🎉我的积分：${data.data.income_info.score_balance}金币`)//打印返回成功數值        
                } else {
 
                 }
@@ -590,5 +608,35 @@ function xgjine(timeout = 0) {
     })
 }
 
+//西瓜直播
+function xglive(timeout = 0) {
+    return new Promise((resolve) => {
+
+        let url = {
+            url: XGSPliveurl,
+            headers: JSON.parse(XGSPlivehd),
+            body: `ad_fallback=0`,
+        }
+        $.post(url, async (err, resp, data) => {
+            try {
+
+                data = JSON.parse(data)
+
+                if (data.err_no == 0) {
+    console.log(`🎉直播金币成功：${data.data.amount}金币`)//打印返回成功數值
+           
+               } else {
+    console.log(`❌直播金币失败：${data.err_tips}😿😿`)//打印返回失败数值
+
+                }
+            } catch (e) {
+
+            } finally {
+
+                resolve()
+            }
+        }, timeout)
+    })
+}
 
 function Env(t, e) { class s { constructor(t) { this.env = t } send(t, e = "GET") { t = "string" == typeof t ? { url: t } : t; let s = this.get; return "POST" === e && (s = this.post), new Promise((e, i) => { s.call(this, t, (t, s, r) => { t ? i(t) : e(s) }) }) } get(t) { return this.send.call(this.env, t) } post(t) { return this.send.call(this.env, t, "POST") } } return new class { constructor(t, e) { this.name = t, this.http = new s(this), this.data = null, this.dataFile = "box.dat", this.logs = [], this.isMute = !1, this.isNeedRewrite = !1, this.logSeparator = "\n", this.startTime = (new Date).getTime(), Object.assign(this, e), this.log("", `\ud83d\udd14${this.name}, \u5f00\u59cb!`) } isNode() { return "undefined" != typeof module && !!module.exports } isQuanX() { return "undefined" != typeof $task } isSurge() { return "undefined" != typeof $httpClient && "undefined" == typeof $loon } isLoon() { return "undefined" != typeof $loon } toObj(t, e = null) { try { return JSON.parse(t) } catch { return e } } toStr(t, e = null) { try { return JSON.stringify(t) } catch { return e } } getjson(t, e) { let s = e; const i = this.getdata(t); if (i) try { s = JSON.parse(this.getdata(t)) } catch { } return s } setjson(t, e) { try { return this.setdata(JSON.stringify(t), e) } catch { return !1 } } getScript(t) { return new Promise(e => { this.get({ url: t }, (t, s, i) => e(i)) }) } runScript(t, e) { return new Promise(s => { let i = this.getdata("@chavy_boxjs_userCfgs.httpapi"); i = i ? i.replace(/\n/g, "").trim() : i; let r = this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout"); r = r ? 1 * r : 20, r = e && e.timeout ? e.timeout : r; const [o, h] = i.split("@"), a = { url: `http://${h}/v1/scripting/evaluate`, body: { script_text: t, mock_type: "cron", timeout: r }, headers: { "X-Key": o, Accept: "*/*" } }; this.post(a, (t, e, i) => s(i)) }).catch(t => this.logErr(t)) } loaddata() { if (!this.isNode()) return {}; { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e); if (!s && !i) return {}; { const i = s ? t : e; try { return JSON.parse(this.fs.readFileSync(i)) } catch (t) { return {} } } } } writedata() { if (this.isNode()) { this.fs = this.fs ? this.fs : require("fs"), this.path = this.path ? this.path : require("path"); const t = this.path.resolve(this.dataFile), e = this.path.resolve(process.cwd(), this.dataFile), s = this.fs.existsSync(t), i = !s && this.fs.existsSync(e), r = JSON.stringify(this.data); s ? this.fs.writeFileSync(t, r) : i ? this.fs.writeFileSync(e, r) : this.fs.writeFileSync(t, r) } } lodash_get(t, e, s) { const i = e.replace(/\[(\d+)\]/g, ".$1").split("."); let r = t; for (const t of i) if (r = Object(r)[t], void 0 === r) return s; return r } lodash_set(t, e, s) { return Object(t) !== t ? t : (Array.isArray(e) || (e = e.toString().match(/[^.[\]]+/g) || []), e.slice(0, -1).reduce((t, s, i) => Object(t[s]) === t[s] ? t[s] : t[s] = Math.abs(e[i + 1]) >> 0 == +e[i + 1] ? [] : {}, t)[e[e.length - 1]] = s, t) } getdata(t) { let e = this.getval(t); if (/^@/.test(t)) { const [, s, i] = /^@(.*?)\.(.*?)$/.exec(t), r = s ? this.getval(s) : ""; if (r) try { const t = JSON.parse(r); e = t ? this.lodash_get(t, i, "") : e } catch (t) { e = "" } } return e } setdata(t, e) { let s = !1; if (/^@/.test(e)) { const [, i, r] = /^@(.*?)\.(.*?)$/.exec(e), o = this.getval(i), h = i ? "null" === o ? null : o || "{}" : "{}"; try { const e = JSON.parse(h); this.lodash_set(e, r, t), s = this.setval(JSON.stringify(e), i) } catch (e) { const o = {}; this.lodash_set(o, r, t), s = this.setval(JSON.stringify(o), i) } } else s = this.setval(t, e); return s } getval(t) { return this.isSurge() || this.isLoon() ? $persistentStore.read(t) : this.isQuanX() ? $prefs.valueForKey(t) : this.isNode() ? (this.data = this.loaddata(), this.data[t]) : this.data && this.data[t] || null } setval(t, e) { return this.isSurge() || this.isLoon() ? $persistentStore.write(t, e) : this.isQuanX() ? $prefs.setValueForKey(t, e) : this.isNode() ? (this.data = this.loaddata(), this.data[e] = t, this.writedata(), !0) : this.data && this.data[e] || null } initGotEnv(t) { this.got = this.got ? this.got : require("got"), this.cktough = this.cktough ? this.cktough : require("tough-cookie"), this.ckjar = this.ckjar ? this.ckjar : new this.cktough.CookieJar, t && (t.headers = t.headers ? t.headers : {}, void 0 === t.headers.Cookie && void 0 === t.cookieJar && (t.cookieJar = this.ckjar)) } get(t, e = (() => { })) { t.headers && (delete t.headers["Content-Type"], delete t.headers["Content-Length"]), this.isSurge() || this.isLoon() ? (this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.get(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) })) : this.isQuanX() ? (this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t))) : this.isNode() && (this.initGotEnv(t), this.got(t).on("redirect", (t, e) => { try { if (t.headers["set-cookie"]) { const s = t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString(); this.ckjar.setCookieSync(s, null), e.cookieJar = this.ckjar } } catch (t) { this.logErr(t) } }).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) })) } post(t, e = (() => { })) { if (t.body && t.headers && !t.headers["Content-Type"] && (t.headers["Content-Type"] = "application/x-www-form-urlencoded"), t.headers && delete t.headers["Content-Length"], this.isSurge() || this.isLoon()) this.isSurge() && this.isNeedRewrite && (t.headers = t.headers || {}, Object.assign(t.headers, { "X-Surge-Skip-Scripting": !1 })), $httpClient.post(t, (t, s, i) => { !t && s && (s.body = i, s.statusCode = s.status), e(t, s, i) }); else if (this.isQuanX()) t.method = "POST", this.isNeedRewrite && (t.opts = t.opts || {}, Object.assign(t.opts, { hints: !1 })), $task.fetch(t).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => e(t)); else if (this.isNode()) { this.initGotEnv(t); const { url: s, ...i } = t; this.got.post(s, i).then(t => { const { statusCode: s, statusCode: i, headers: r, body: o } = t; e(null, { status: s, statusCode: i, headers: r, body: o }, o) }, t => { const { message: s, response: i } = t; e(s, i, i && i.body) }) } } time(t) { let e = { "M+": (new Date).getMonth() + 1, "d+": (new Date).getDate(), "H+": (new Date).getHours(), "m+": (new Date).getMinutes(), "s+": (new Date).getSeconds(), "q+": Math.floor(((new Date).getMonth() + 3) / 3), S: (new Date).getMilliseconds() }; /(y+)/.test(t) && (t = t.replace(RegExp.$1, ((new Date).getFullYear() + "").substr(4 - RegExp.$1.length))); for (let s in e) new RegExp("(" + s + ")").test(t) && (t = t.replace(RegExp.$1, 1 == RegExp.$1.length ? e[s] : ("00" + e[s]).substr(("" + e[s]).length))); return t } msg(e = t, s = "", i = "", r) { const o = t => { if (!t) return t; if ("string" == typeof t) return this.isLoon() ? t : this.isQuanX() ? { "open-url": t } : this.isSurge() ? { url: t } : void 0; if ("object" == typeof t) { if (this.isLoon()) { let e = t.openUrl || t.url || t["open-url"], s = t.mediaUrl || t["media-url"]; return { openUrl: e, mediaUrl: s } } if (this.isQuanX()) { let e = t["open-url"] || t.url || t.openUrl, s = t["media-url"] || t.mediaUrl; return { "open-url": e, "media-url": s } } if (this.isSurge()) { let e = t.url || t.openUrl || t["open-url"]; return { url: e } } } }; this.isMute || (this.isSurge() || this.isLoon() ? $notification.post(e, s, i, o(r)) : this.isQuanX() && $notify(e, s, i, o(r))); let h = ["", "==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="]; h.push(e), s && h.push(s), i && h.push(i), console.log(h.join("\n")), this.logs = this.logs.concat(h) } log(...t) { t.length > 0 && (this.logs = [...this.logs, ...t]), console.log(t.join(this.logSeparator)) } logErr(t, e) { const s = !this.isSurge() && !this.isQuanX() && !this.isLoon(); s ? this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t.stack) : this.log("", `\u2757\ufe0f${this.name}, \u9519\u8bef!`, t) } wait(t) { return new Promise(e => setTimeout(e, t)) } done(t = {}) { const e = (new Date).getTime(), s = (e - this.startTime) / 1e3; this.log("", `\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`), this.log(), (this.isSurge() || this.isQuanX() || this.isLoon()) && $done(t) } }(t, e) }
