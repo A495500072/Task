0cac25ac8e7e6eb06c8fbca3f574d7ba7b77c784
XWF888 <77550098+XWF888@users.noreply.github.com>
2022-05-22 09:28:19 +0800

    Update xgspyd.js

--- xgsp/xgspyd.js
+++ xgsp/xgspyd.js
@@ -139,7 +139,7 @@ console.log(`\n\n===============================================            ⏰�
             //await xgqd()//你要执行的版块
             //await $.wait(1000)//你要延迟时间1000=1秒
 //循环运行多次任务
-  for (let c = 0; c < 50; c++) {
+  for (let c = 0; c < 55; c++) {
            $.index = c + 1
             await xgyd()  
             await $.wait(28000)
